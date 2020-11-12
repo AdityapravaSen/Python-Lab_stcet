e3e8406d4925cb9ca1a0d456d466628e55eb3203
Adityaprava Sen <adityapravasen0911@gmail.com>
2020-11-12 12:23:45 +0530

    assignment 6

---
+++ Assignment 6/1.py
@@ -0,0 +1,43 @@
+st = input("enter the string")
+
+digit = 0
+
+vowel = 0
+
+consonant = 0
+
+special = 0
+
+for i in range(0, len(st)):
+
+    ch = st[i]
+
+    if(ch >= 'a' and ch <= 'z')or(ch >= 'A' and ch <= 'Z'):
+
+        ch = ch.lower()
+
+        if(ch == 'a' or ch == 'i' or ch == 'i' or ch == 'o' or ch == 'u'):
+
+            vowel = vowel+1
+
+        else:
+
+            consonant = consonant+1
+
+    elif(ch >= '0' and ch <= '9'):
+
+        digit = digit+1
+
+    else:
+
+        special = special+1
+
+print("string:", st)
+
+print("vowel:", vowel)
+
+print("consonant:", consonant)
+
+print("digit:", digit)
+
+print("special character:", special)
